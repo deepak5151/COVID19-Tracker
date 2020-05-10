7db22473c8a2c7942189e4cd04c744a0e3c98600
Deepak Kapoor <nk93555@gmail.com>
2020-05-10 12:11:23 +0530

    Updates

---
+++ precache-manifest.b8f55fcc02d16e69d1c4efb90dd0b175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cb67f2bf2966dbd2a90037d71a6ba0",
+    "url": "/COVID19-Tracker/index.html"
+  },
+  {
+    "revision": "f1032ee519122474f43c",
+    "url": "/COVID19-Tracker/static/css/main.b3239ca4.chunk.css"
+  },
+  {
+    "revision": "ae6350392b8f768ac61f",
+    "url": "/COVID19-Tracker/static/js/2.7c593a6b.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/COVID19-Tracker/static/js/2.7c593a6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1032ee519122474f43c",
+    "url": "/COVID19-Tracker/static/js/main.06430c03.chunk.js"
+  },
+  {
+    "revision": "5f8cd3dc51a52541abe1",
+    "url": "/COVID19-Tracker/static/js/runtime-main.01ee2b36.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID19-Tracker/static/media/covid19.d7265326.png"
+  }
+]);
